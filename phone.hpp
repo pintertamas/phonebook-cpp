34c5b1900a79cc4d1f260247592cfd4193bd4bda
Tomi <pintertamas99@gmail.com>
2020-04-27 18:53:07 +0200

    dokumentálás folyik kész a phone.hpp

--- phone.hpp
+++ phone.hpp
@@ -14,36 +14,82 @@
  * @file phone.hpp
  * @brief Phone class fügvényei
  */
-
+/**
+ * @brief Work class létrehozása
+ */
 class Work : public Contact {
     String company;
     String website;
 public:
-    explicit Work(const String &number = "unknown", const String &name = "unknown", const String &email = "unknown", const String &company = "unknown",
-         const String &website = "unknown") : Contact(number, name, email), company(company), website(website) {}
+    /**
+     * @brief Work CTOR
+     * A megadott adatokkal létrehoz egy work kontaktot
+     */
+    explicit Work(const String &number = "unknown", const String &name = "unknown", const String &email = "unknown",
+                  const String &company = "unknown",
+                  const String &website = "unknown") : Contact(number, name, email), company(company),
+                                                       website(website) {}
 
+    /**
+     * @brief Company getter
+     * @return company
+     */
     String getCompany() const { return company; }
 
+    /**
+     * @brief Website getter
+     * @return website
+     */
     String getWebsite() const { return website; }
 
-    std::ostream &toString(std::ostream&) override;
+    /**
+     * @brief Kiírja a megadott kimenetre a work adatait formázva
+     * @return work adattagjai formázva
+     */
+    std::ostream &toString(std::ostream &) override;
 };
 
+/**
+ * @brief Private class létrehozása
+ */
 class Private : public Contact {
     String address;
     String nickname;
     int birthday;
 public:
-    explicit Private(const String &number = "unknown", const String &name = "unknown", const String &nickname = "unknown", const String &email = "unknown", const String &address = "unknown",
-            int birthday = 18900101) : Contact(number, name, email), address(address), nickname(nickname), birthday(birthday) {}
+    /**
+     * @brief Private CTOR
+     * A megadott paraméterekkel létrehoöz egy private kontaktot
+     */
+    explicit Private(const String &number = "unknown", const String &name = "unknown",
+                     const String &nickname = "unknown", const String &email = "unknown",
+                     const String &address = "unknown",
+                     int birthday = 18900101) : Contact(number, name, email), address(address), nickname(nickname),
+                                                birthday(birthday) {}
 
+    /**
+     * @brief Birthday getter
+     * @return birthday
+     */
     int getBirthday() const { return birthday; }
 
+    /**
+     * @brief Nickname getter
+     * @return nickname
+     */
     String getNickname() const { return nickname; };
 
+    /**
+     * @brief Addressg getter
+     * @return address
+     */
     String getAddress() const { return address; }
 
-    std::ostream &toString(std::ostream&) override;
+    /**
+    * @brief Kiírja a megadott kimenetre a private adatait formázva
+    * @return private adattagjai formázva
+    */
+    std::ostream &toString(std::ostream &) override;
 };
 
 #endif //MY_AWESOME_PHONEBOOK_PHONE_HPP
