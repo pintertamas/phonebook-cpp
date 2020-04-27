b86d21cc06d39a8260ce5f4002c30974151382db
Tomi <pintertamas99@gmail.com>
2020-04-27 18:46:58 +0200

    dokumentálás folyik kész a contact.hpp

--- contact.hpp
+++ contact.hpp
@@ -11,24 +11,52 @@
  * @file contact.hpp
  * @brief Contact class
  */
-
+/**
+ * @brief Contact class létrehozása
+ */
 class Contact {
+    /**
+     * @brief A közös tulajdonságok
+     * number, name, email
+     */
 protected:
     String number;
     String name;
     String email;
 public:
+    /**
+     * @brief Contact CTOR
+     * A megadott értékekkel létrehoz egy Contactot
+     */
     Contact(const String &number, const String &name, const String &email)
             : number(number), name(name), email(email) {};
 
+    /**
+     * @brief Number getter
+     * @return number
+     */
     String getNumber() const { return number; };
 
+    /**
+     * @brief Name getter
+     * @return name
+     */
     String getName() const { return name; };
 
+    /**
+     * @brief Email getter
+     * @return email
+     */
     String getEmail() const { return email; };
 
+    /**
+     * @brief A toString tiszta virtuális függvénye
+     */
     virtual std::ostream &toString(std::ostream &) = 0;
 
+    /**
+     * @brief Contact DTOR
+     */
     virtual ~Contact() = default;
 };
 
